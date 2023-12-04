4856a1e01769f843431af17d6761581ee86c0f98
Douglas dos Santos Oliveira <doug1306@gmail.com>
2023-12-03 22:28:40 -0300

    feat: add logo and input search

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Sidebar } from '../components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="grid-cols-app grid min-h-screen">
-          <aside className="border-r border-zinc-200 px-5 py-8">aide</aside>
+          <Sidebar />
           <main className="px-4 pb-12 pt-8">{children}</main>
         </div>
       </body>
