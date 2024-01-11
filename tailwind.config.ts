c14508f03c89743c6379178a516eda48546cb205
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-01-11 19:18:16 -0300

    feat: Upload do Avatar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,22 @@ const config: Config = {
     extend: {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
-        profile: 'min-content 1fr min-content',
+        profile: 'max-content 1fr max-content',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+
+      borderWidth: {
+        6: '6px',
+      },
+
+      colors: {
+        violet: {
+          25: '#fcfaff',
+        },
+      },
+
+      maxWidth: {
+        app: '700px',
       },
     },
   },
