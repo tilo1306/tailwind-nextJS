74680f5c6daac7a02d2bdc8d6daeddbfcdede38a
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-07 15:26:36 -0300

    feat: Componentizing Select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import * as Input from '@/components/Input'
 import * as FileInput from '@/components/Form/FileInput'
 import { Mail } from 'lucide-react'
 import { Select } from '@/components/Form/Select'
+import { SelectItem } from '@/components/Form/Select/SelectItem'
 
 export default function Home() {
   return (
@@ -108,7 +109,10 @@ export default function Home() {
             >
               Country
             </label>
-            <Select />
+            <Select placeholder="Select a country...">
+              <SelectItem value="br" text="Brazil" />
+              <SelectItem value="us" text="United State" />
+            </Select>
             <div />
           </div>
           <div className="grid grid-cols-form gap-3 pt-5">
@@ -118,6 +122,13 @@ export default function Home() {
             >
               Timezone
             </label>
+            <Select placeholder="Select a timezone...">
+              <SelectItem
+                value="utc8"
+                text="Pacific Standard Time (UTC-08:00)"
+              />
+              <SelectItem value="utc3" text="America São Paulo (UTC-03:00)" />
+            </Select>
             <div />
           </div>
           <div className="grid grid-cols-form gap-3 pt-5">
