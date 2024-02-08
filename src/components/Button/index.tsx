18a71baa25e38b3c4a9af5fccd88339062c74ff4
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-08 15:30:34 -0300

    feat: Variants with slots

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,13 +7,15 @@ const button = tv({
     'focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-violet-500',
     'active:opacity-80',
   ],
+
   variants: {
     variant: {
-      primary: 'bg-violet-600 text-white hover:bg-biolet-700',
-      ghost: 'rounded-md px-2 hover:bg-zinc-50 shadow-none',
+      primary: 'bg-violet-600 text-white hover:bg-violet-700',
+      ghost: 'rounded-md px-2 hover:bg-zinc-50 shadow-none text-zinc-500',
       outline: 'border border-zinc-300 text-zinc-700 hover:bg-zinc-50',
     },
   },
+
   defaultVariants: {
     variant: 'primary',
   },
@@ -21,6 +23,6 @@ const button = tv({
 
 export type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
 
-export function Button({ variant, ...props }: ButtonProps) {
-  return <button className={button({ variant })} {...props} />
+export function Button({ variant, className, ...props }: ButtonProps) {
+  return <button {...props} className={button({ variant, className })} />
 }
