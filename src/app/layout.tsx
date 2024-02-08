f98d40883928777d6ae54d3a203c1461ef80513a
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-08 17:00:20 -0300

    feat: darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,12 +16,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="antialiased">
+    <html lang="en" className="dark antialiased">
       <body className={inter.className}>
-        <main className="grid min-h-screen grid-cols-app">
+        <div className="min-h-screen dark:bg-zinc-900 lg:grid lg:grid-cols-app">
           <Sidebar />
-          <div className="px-8 pb-12 pt-8">{children}</div>
-        </main>
+          <main className="max-w-[100vw] px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   )
--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,21 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
+import { Button } from '@/components/Button'
 import { LogOut } from 'lucide-react'
-import { Button } from '../Button'
 
 export function Profile() {
   return (
-    <div className="flex grid-cols-profile items-center gap-3">
+    <div className="grid grid-cols-profile items-center gap-3">
       <img
+        src="https://github.com/diego3g.png"
         className="h-10 w-10 rounded-full"
-        src="https://github.com/tilo1306.png"
-        alt="progile"
+        alt=""
       />
-      <div className="flex flex-1 flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
-          Douglas S Oliveira
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
+          Diego Fernandes
         </span>
-        <span className="truncate text-sm text-zinc-500">
-          doug1306@gmail.com
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
+          diego@rocketseat.com.br
         </span>
       </div>
       <Button type="button" variant="ghost">
